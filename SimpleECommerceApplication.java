7b30315294d23fc98e9f154127423743d6723391
Aswin-git1 <aswinkhatiwada477@gmail.com>
2025-02-06 08:52:39 +0545

    Add files via upload

---
+++ SimpleECommerceApplication.java
@@ -0,0 +1,13 @@
+package Simple.E_Commerce.Simple.E_Commerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleECommerceApplication.class, args);
+	}
+
+}
